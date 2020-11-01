ffd40c19e75697952de72ba4c507f3b2351a8b8c
s11283 <s11283@pjwstk.edu.pl>
2020-11-01 19:03:42 +0100

    Pierwszy commit

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 
 namespace cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+
+            if (response.IsSuccessStatusCode) {
+
+                var html = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+@[a-z.]+");
+
+                MatchCollection mailAdresses = regex.Matches(html);
+
+                foreach (var i in mailAdresses) {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine("koniec");
+            }
+
+            
         }
     }
 }
